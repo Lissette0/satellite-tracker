09d6267bff0fd767b40141396343119d5901bfef
alanyu108 <alanwong103@gmail.com>
2021-09-20 23:40:24 -0400

    textures
    added earth model to front page

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import Navbar from './components/Navbar';
-import{BrowserRouter as Router,Switch, Route} from 'react-router-dom'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import './App.css';
 import Home from './components/pages/Home';
 import Earth from './components/Earthmodel'
+import Loading from "./components/Loading"
+
 
 function App() {
   return (
     <>
-    <div className="App">
-      <Router>
-        <Navbar/>
-        <Switch>
-        <Route path='/' exact component={Home} />
-        </Switch>
-      </Router>
-    </div>
-    <Earth/>
+      <div className="App">
+        <Router>
+          <Navbar />
+          <Switch>
+            <Route path='/' exact component={Home} />
+          </Switch>
+        </Router>
+      </div>
+      <Suspense fallback={Loading} >
+        <Earth />
+      </Suspense>
     </>
   );
 }
--- src/components/Earthmodel.js
+++ src/components/Earthmodel.js
@@ -1,24 +1,63 @@
 // 3d earth model component
-import React from 'react'
-import {Canvas} from '@react-three/fiber'
+import * as THREE from "three";
+import React, { useRef } from 'react'
+import { TextureLoader } from 'three'
+import { OrbitControls } from '@react-three/drei'
+import CloudsTexture from './assets/textures/8k_clouds.jpg'
+import { Canvas, useLoader, useFrame } from '@react-three/fiber'
+import EarthTexture from './assets/textures/8k_earth_daymap.jpg'
+import EarthNormalTexture from './assets/textures/8k_earth_normal_map.jpg'
+import EarthSpecularTexture from './assets/textures/8k_earth_specular_map.jpg'
 
 
-export default function Earth() 
-{
+
+function Sphere({ position, texture }) {
+    const EarthMesh = useRef(null)
+    const CloudsMesh = useRef(null)
+    useFrame(() => {
+        EarthMesh.current.rotation.y += 0.001
+        CloudsMesh.current.rotation.y += 0.001
+    })
+    const [Earth, NormalEarth, SpecularEarth, Clouds] = useLoader(TextureLoader, [texture, EarthNormalTexture, EarthSpecularTexture, CloudsTexture])
     return (
-        <Canvas>
-            <directionalLight color = {"white"} intensity = {1.5} position = {[0,10,0]}/>
-            <mesh position = {[0,0,0]}>
-                <sphereBufferGeometry attach = "geometry" args ={[2,32,16]}  />
-                <ambientLight intensity={0.5} />
+        <>
+            <ambientLight intensity={0.5} />
+            <mesh ref={EarthMesh} position={position}>
+                <sphereGeometry attach="geometry" args={[2, 32, 32]} />
+                <meshPhongMaterial specularMap={SpecularEarth} />
                 <meshStandardMaterial
-                    transparent
-                    attach = "material" 
-                    color = 'red'
-                    roughness = {0.1}
-                    metalness = {0.5}
+                    map={Earth}
+                    normalMap={NormalEarth}
+                    attach="material"
                 />
             </mesh>
+            <mesh ref={CloudsMesh} position={position} >
+                <sphereGeometry attach="geometry" args={[2.04, 32, 32]} />
+                <meshPhongMaterial
+                    map={Clouds}
+                    opacity={0.4}
+                    depthWrite={true}
+                    transparent={true}
+                    side={THREE.DoubleSide}
+                />
+            </mesh>
+            <OrbitControls
+                enableZoom={true}
+                enableRotate={true}
+                enablePan={true}
+                rotateSpeed={0.4}
+            />
+        </>
+
+    )
+}
+
+export default function Earth() {
+    return (
+        <Canvas>
+            <color attach="background" args={["black"]} />
+            <directionalLight color="#f6f3ea" intensity={2} position={[-3, 3, 1]} />
+            <Sphere position={[0, 0, 0]} texture={EarthTexture} />
         </Canvas>
     )
 }
